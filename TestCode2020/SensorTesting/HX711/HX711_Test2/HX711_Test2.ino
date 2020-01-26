f3a9fd813ca17fabfd8059a3e5a04e4431841b35
Andrew Hellrigel <ahellrigel33@gmail.com>
2020-01-26 00:11:09 -0500

    pre-merge

--- TestCode2020/SensorTesting/HX711/HX711_Test2/HX711_Test2.ino
+++ TestCode2020/SensorTesting/HX711/HX711_Test2/HX711_Test2.ino
@@ -1,16 +1,16 @@
 #include "HX711.h"
-#include <SD.h>
+//#include <SD.h>
 
 
 // HX711 circuit wiring
 const int LOADCELL_SDA_PIN = 18;
 const int LOADCELL_SCK_PIN = 19;
 
-File dataFile;
-const int chipSelect = BUILTIN_SDCARD;
-bool hasSD = false; //Keep false, will auto-set to true if it detects an SD card present
-uint16_t sdInterval     = 50  * 1000; //us  (50  ms)
-uint64_t sdTime = micros();
+//File dataFile;
+//const int chipSelect = BUILTIN_SDCARD;
+//bool hasSD = false; //Keep false, will auto-set to true if it detects an SD card present
+//uint16_t sdInterval     = 50  * 1000; //us  (50  ms)
+//uint64_t sdTime = micros();
 
 HX711 scale;
 
@@ -19,12 +19,12 @@ void setup() {
   // Initialize library with data output pin, clock input pin and gain factor.
   scale.begin(LOADCELL_SDA_PIN, LOADCELL_SCK_PIN);
 
-  if (!SD.begin(chipSelect)) {
-    Serial.println("SD card initialization failed");
-  } else {
-    Serial.println("SD card initialization successful");
-    hasSD = true;
-  }
+//  if (!SD.begin(chipSelect)) {
+//    Serial.println("SD card initialization failed");
+//  } else {
+//    Serial.println("SD card initialization successful");
+//    hasSD = true;
+//  }
   delay(1000);
   scale.set_scale(2280.f);                      // this value is obtained by calibrating the scale with known weights; see the README for details
   scale.tare();				        // reset the scale to 0
@@ -35,18 +35,18 @@ void loop() {
   //Serial.print("one reading:\t");
   Serial.println(scale.get_units(), 1);
   
-  if (abs(micros() - sdTime) > sdInterval && hasSD){
-    dataFile = SD.open("COFTEST1.txt", FILE_WRITE); //---------CHANGE FILE NAME HERE---------//
-    if (dataFile) {
-      Serial.println(scale.get_units(), 1);
-      dataFile.print(scale.get_units(), 1);
-      dataFile.println();
-    } else {
-      Serial.println("Error opening test.txt");
-    }
-    dataFile.close();
-    sdTime = micros();
-  }  
+//  if (abs(micros() - sdTime) > sdInterval && hasSD){
+//    dataFile = SD.open("COFTEST1.txt", FILE_WRITE); //---------CHANGE FILE NAME HERE---------//
+//    if (dataFile) {
+//      Serial.println(scale.get_units(), 1);
+//      dataFile.print(scale.get_units(), 1);
+//      dataFile.println();
+//    } else {
+//      Serial.println("Error opening test.txt");
+//    }
+//    dataFile.close();
+//    sdTime = micros();
+//  }  
 
   //scale.power_down();			        // put the ADC in sleep mode
   //delay(5);
