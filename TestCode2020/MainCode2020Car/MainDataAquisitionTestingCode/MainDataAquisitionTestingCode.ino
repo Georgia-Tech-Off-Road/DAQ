e44cddb7979cc93a90846d3ce0d9cb2cba2f299f
Andrew Hellrigel <ahellrigel33@gmail.com>
2020-02-15 02:08:25 -0500

    Updated DAQ testing Code actually this time

--- TestCode2020/MainCode2020Car/MainDataAquisitionTestingCode/MainDataAquisitionTestingCode.ino
+++ TestCode2020/MainCode2020Car/MainDataAquisitionTestingCode/MainDataAquisitionTestingCode.ino
@@ -14,18 +14,22 @@ struct Data{
   uint8_t bls;   //Value needs to be bigger if travel is more than 255mm
   uint8_t steer; //Value needs to be bigger if travel is more than 255mm
   uint8_t ecvt;  //Value needs to be bigger if travel is more than 255mm
+  uint8_t throttle; //Throttle position ###Parameters currently unknown###
   uint16_t espd; //Engine speed in rpm
   uint16_t wspd; //Wheel speed in rpm
   uint16_t brkf; //Brake pressure in psi
-  uint16_t brkfr;//Brake pressure reference in psi
   uint16_t brkb; //Brake pressure in psi
-  uint16_t brkbr;//Brake pressure reference in psi
 
   imu::Quaternion imu1;
   imu::Quaternion imu2;
   imu::Vector<3> accel1;
   imu::Vector<3> accel2;
+  imu::Vector<3> gyro1;
+  imu::Vector<3> gyro2;
 
+  uint8_t hrs; //RTC hours (0-24)
+  uint8_t mins; //RTC minutes (0-60)
+  uint8_t secs; //RTC seconds (0-60)
   uint32_t atime; //Time in microseconds
 
   uint8_t allDataSD[64] = {0};
@@ -44,46 +48,46 @@ struct Data{
     allDataSD[9] = (byte) wspd;
     allDataSD[10] = (byte) brkf >> 8;
     allDataSD[11] = (byte) brkf;
-    allDataSD[12] = (byte) brkfr >> 8;
-    allDataSD[13] = (byte) brkfr;
-    allDataSD[14] = (byte) brkb >> 8;
-    allDataSD[15] = (byte) brkb;
-    allDataSD[16] = (byte) brkbr >> 8;
-    allDataSD[17] = (byte) brkbr;
-    allDataSD[18] = (byte)((int16_t)imu1.w()*(1 << 14)) >> 8;
-    allDataSD[19] = (byte)((int16_t)imu1.w()*(1 << 14));
-    allDataSD[20] = (byte)((int16_t)imu1.x()*(1 << 14)) >> 8;
-    allDataSD[21] = (byte)((int16_t)imu1.x()*(1 << 14));
-    allDataSD[22] = (byte)((int16_t)imu1.y()*(1 << 14)) >> 8;
-    allDataSD[23] = (byte)((int16_t)imu1.y()*(1 << 14));
-    allDataSD[24] = (byte)((int16_t)imu1.z()*(1 << 14)) >> 8;
-    allDataSD[25] = (byte)((int16_t)imu1.z()*(1 << 14));
-    allDataSD[26] = (byte)((int16_t)imu2.w()*(1 << 14)) >> 8;
-    allDataSD[27] = (byte)((int16_t)imu2.w()*(1 << 14));
-    allDataSD[28] = (byte)((int16_t)imu2.x()*(1 << 14)) >> 8;
-    allDataSD[29] = (byte)((int16_t)imu2.x()*(1 << 14));
-    allDataSD[30] = (byte)((int16_t)imu2.y()*(1 << 14)) >> 8;
-    allDataSD[31] = (byte)((int16_t)imu2.y()*(1 << 14));
-    allDataSD[32] = (byte)((int16_t)imu2.z()*(1 << 14)) >> 8;
-    allDataSD[33] = (byte)((int16_t)imu2.z()*(1 << 14));
-    allDataSD[34] = (byte)((int16_t)accel1.x()*100) >> 8;
-    allDataSD[35] = (byte)((int16_t)accel1.x()*100);
-    allDataSD[36] = (byte)((int16_t)accel1.y()*100) >> 8;
-    allDataSD[37] = (byte)((int16_t)accel1.y()*100);
-    allDataSD[38] = (byte)((int16_t)accel1.z()*100) >> 8;
-    allDataSD[39] = (byte)((int16_t)accel1.z()*100);
-    allDataSD[40] = (byte)((int16_t)accel2.x()*100) >> 8;
-    allDataSD[41] = (byte)((int16_t)accel2.x()*100);
-    allDataSD[42] = (byte)((int16_t)accel2.y()*100) >> 8;
-    allDataSD[43] = (byte)((int16_t)accel2.y()*100);
-    allDataSD[44] = (byte)((int16_t)accel2.z()*100) >> 8;
-    allDataSD[45] = (byte)((int16_t)accel2.z()*100);
-    allDataSD[46] = 0;
-    allDataSD[47] = 0;
-    allDataSD[48] = 0;
-    allDataSD[49] = 0;
-    allDataSD[50] = 0;
-    allDataSD[51] = 0;
+    allDataSD[12] = (byte) brkb >> 8;;
+    allDataSD[13] = (byte) brkb;
+    allDataSD[14] = throttle;
+    allDataSD[15] = hrs;
+    allDataSD[16] = mins;
+    allDataSD[17] = secs;
+    allDataSD[18] = (byte)((int16_t)(imu1.w()*(1 << 14))) >> 8;
+    allDataSD[19] = (byte)((int16_t)(imu1.w()*(1 << 14)));
+    allDataSD[20] = (byte)((int16_t)(imu1.x()*(1 << 14))) >> 8;
+    allDataSD[21] = (byte)((int16_t)(imu1.x()*(1 << 14)));
+    allDataSD[22] = (byte)((int16_t)(imu1.y()*(1 << 14))) >> 8;
+    allDataSD[23] = (byte)((int16_t)(imu1.y()*(1 << 14)));
+    allDataSD[24] = (byte)((int16_t)(imu1.z()*(1 << 14))) >> 8;
+    allDataSD[25] = (byte)((int16_t)(imu1.z()*(1 << 14)));
+    allDataSD[26] = (byte)((int16_t)(imu2.w()*(1 << 14))) >> 8;
+    allDataSD[27] = (byte)((int16_t)(imu2.w()*(1 << 14)));
+    allDataSD[28] = (byte)((int16_t)(imu2.x()*(1 << 14))) >> 8;
+    allDataSD[29] = (byte)((int16_t)(imu2.x()*(1 << 14)));
+    allDataSD[30] = (byte)((int16_t)(imu2.y()*(1 << 14))) >> 8;
+    allDataSD[31] = (byte)((int16_t)(imu2.y()*(1 << 14)));
+    allDataSD[32] = (byte)((int16_t)(imu2.z()*(1 << 14))) >> 8;
+    allDataSD[33] = (byte)((int16_t)(imu2.z()*(1 << 14)));
+    allDataSD[34] = (byte)((int16_t)(accel1.x()*100)) >> 8;
+    allDataSD[35] = (byte)((int16_t)(accel1.x()*100));
+    allDataSD[36] = (byte)((int16_t)(accel1.y()*100)) >> 8;
+    allDataSD[37] = (byte)((int16_t)(accel1.y()*100));
+    allDataSD[38] = (byte)((int16_t)(accel1.z()*100)) >> 8;
+    allDataSD[39] = (byte)((int16_t)(accel1.z()*100));
+    allDataSD[40] = (byte)((int16_t)(accel2.x()*100)) >> 8;
+    allDataSD[41] = (byte)((int16_t)(accel2.x()*100));
+    allDataSD[42] = (byte)((int16_t)(accel2.y()*100)) >> 8;
+    allDataSD[43] = (byte)((int16_t)(accel2.y()*100));
+    allDataSD[44] = (byte)((int16_t)(accel2.z()*100)) >> 8;
+    allDataSD[45] = (byte)((int16_t)(accel2.z()*100));
+    allDataSD[46] = (byte)((int16_t)(gyro1.x()*100)) >> 8;
+    allDataSD[47] = (byte)((int16_t)(gyro1.x()*100));
+    allDataSD[48] = (byte)((int16_t)(gyro1.y()*100)) >> 8;
+    allDataSD[49] = (byte)((int16_t)(gyro1.y()*100));
+    allDataSD[50] = (byte)((int16_t)(gyro1.z()*100)) >> 8;
+    allDataSD[51] = (byte)((int16_t)(gyro1.z()*100));
     allDataSD[52] = (byte) atime >> 56;
     allDataSD[53] = (byte) atime >> 48;
     allDataSD[54] = (byte) atime >> 40;
@@ -121,12 +125,13 @@ uint16_t serialInterval = 50 * 1000; //us  (50  ms)
 uint32_t xbeeTime = micros(), sdTime = micros(), serialTime = micros(), oldTime = micros();
 
 
-LDS FRShock(A0, 200), FLShock(A1, 200), BRShock(A2, 200), BLShock(A3, 200), Steer(A4, 50), ECVT(A5, 50); //inputPin, travelMM, isReversed = false
-PressureSensor BrakeFront(A6, 2000), BrakeFrontReference(A7, 2000), BrakeBack(A8, 2000), BrakeBackReference(A9, 2000); //inputPin, scale (PSI)
-HallEffectSpeedSensor EngineSpeed(6, 20), WheelSpeed(10, 20); //inputPin, toneWheelTeeth, intervalLength = 50us, averagingAmount = 200
+LDS FRShock(A0, 225), FLShock(A2, 225, true), BRShock(A1, 200), BLShock(A3, 200), Steer(A6, 50), ECVT(A14, 50); //inputPin, travelMM, isReversed = false
+PressureSensor BrakeFront(A12, 2000, 4600, 8200), BrakeBack(A13, 2000, 4600, 8200); //inputPin, maxPressure (PSI), r1 (Ohms), r2 (Ohms)
+HallEffectSpeedSensor EngineSpeed(6, 20), WheelSpeed(5, 20); //inputPin, toneWheelTeeth, intervalLength = 50us, averagingAmount = 200
 Adafruit_BNO055 bno1 = Adafruit_BNO055(28, BNO055_ADDRESS_A); //0x28 address (ADR pin is left with default pull down resistor)
 Adafruit_BNO055 bno2 = Adafruit_BNO055(29, BNO055_ADDRESS_B); //0x29 address (ADR pin is pulled high to 3.3V)
 bool hasbno1 = false, hasbno2 = false;
+uint8_t throttlePin = A7;
 
 
 
@@ -141,11 +146,11 @@ void setup() {
   Steer.begin();
   ECVT.begin();
   BrakeFront.begin();
-  BrakeFrontReference.begin();
   BrakeBack.begin();
-  BrakeBackReference.begin();
   EngineSpeed.begin();
   WheelSpeed.begin();
+
+  pinMode(throttlePin, INPUT);
 
   if(!bno1.begin()) {
     Serial.println("BNO055 1 initialization failed");
@@ -229,16 +234,23 @@ void loop() {
 //    Serial.print("BLS: "   + String(data.bls)   + "\t");
 //    Serial.print("Steer: " + String(data.steer) + "\t");
 //    Serial.print("ECVT: "  + String(data.ecvt)  + "\t");
+    Serial.print("Throttle: " + String(data.throttle) + " | ");
 //    Serial.print("BrakeFront: "+ String(data.brkf)  + "\t");
 //    Serial.print("BrakeBack: "+ String(data.brkb)  + "\t");
-    Serial.print("Engine Speed: "+ String(data.espd)  + "\t");
+//    Serial.print("Engine Speed: "+ String(data.espd)  + "\t");
 //    Serial.print("Wheel Speed: " + String(data.wspd)  + "\t");
-//    Serial.print("Roll1: " + String(data.imu1r) + "\t");
-//    Serial.print("Pitch1: " + String(data.imu1p) + "\t");
-//    Serial.print("Yaw1: " + String(data.imu1y) + "\t");
-//    Serial.print("X1: " + String(data.accel1x) + "\t");
-//    Serial.print("Y1: " + String(data.accel1y) + "\t");
-//    Serial.print("Z1: " + String(data.accel1z) + "\t");
+//    Serial.print("X: " + String(data.imu1.x()) + " | ");
+//    Serial.print("Y: " + String(data.imu1.y()) + " | ");
+//    Serial.print("Z: " + String(data.imu1.z()) + " | ");
+//    Serial.print("W: " + String(data.imu1.w()) + " | ");
+    Serial.print("X1: " + String(data.accel1.x()) + " | ");
+    Serial.print("Y1: " + String(data.accel1.y()) + " | ");
+    Serial.print("Z1: " + String(data.accel1.z()) + " | ");
+//    Serial.print(data.accel1.x());
+//    Serial.print(',');
+//    Serial.print(data.accel1.y());
+//    Serial.print(',');
+//    Serial.print(data.accel1.z());
 //    Serial.print("Roll2: " + String(data.imu2r) + "\t");
 //    Serial.print("Pitch2: " + String(data.imu2p) + "\t");
 //    Serial.print("Yaw2: " + String(data.imu2y) + "\t");
@@ -280,17 +292,20 @@ inline void collectAllData(){
   data.espd  = EngineSpeed.getSpeed();
   data.wspd  = WheelSpeed.getSpeed();
   data.brkf  = BrakeFront.getPressurePSI();
-  data.brkfr = BrakeFrontReference.getPressurePSI();
   data.brkb  = BrakeBack.getPressurePSI();
-  data.brkbr = BrakeBackReference.getPressurePSI();
+
+  data.throttle = map(analogRead(throttlePin),0,1023,55,0);
+
 
   if(hasbno1){
     data.imu1  = bno1.getQuat();
-    data.accel1= bno1.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+    data.accel1= bno1.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER); //Make sure to change back to VECTOR_ACCELEROMETER
+    data.gyro1=bno1.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE); 
   }
   if(hasbno2){
     data.imu2  = bno1.getQuat();
     data.accel2= bno1.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+    data.gyro1=bno1.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
   }
 
   data.atime = micros();
