291f59de61184bdfa68ba707e05421bbc9062dce
Peter <pchu34@gatech.edu>
2020-11-01 18:09:21 -0500

    Updated loadcelltest to increase the samplerate for load cell

--- Misc_Projects/Dyno/Arduino code/loadCellTest/loadCellTest.ino
+++ Misc_Projects/Dyno/Arduino code/loadCellTest/loadCellTest.ino
@@ -13,6 +13,11 @@ void setup() {
     Serial.println("Scale not detected. Please check wiring. Freezing...");
   }
   Serial.println("Scale detected!");
+  
+  load.setSampleRate(NAU7802_SPS_320); //Sample rate can be set to 10, 20, 40, 80, or 320Hz
+
+  load.calibrateAFE(); //Does an internal calibration. Recommended after power up, gain changes, sample rate changes, or channel changes.
+
 }
 
 void loop() {
