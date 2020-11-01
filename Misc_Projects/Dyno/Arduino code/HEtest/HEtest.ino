1d85f1ebe1f96fd3f5944c1329cd09e0c3c45bd3
Peter <pchu34@gatech.edu>
2020-11-01 17:02:43 -0500

    Working on HE sensor.ino

--- Misc_Projects/Dyno/Arduino code/HEtest/HEtest.ino
+++ Misc_Projects/Dyno/Arduino code/HEtest/HEtest.ino
@@ -2,13 +2,13 @@
 
 
 
-unsigned long previousTime = micros();
-unsigned long timeInterval = 100000;
+unsigned long previousTime = 0;
+unsigned long timeInterval = 100000000;
 
 int he1pin = 3;
 int he2pin = 4;
 int he3pin = 5;
-uint16_t ppm = 500;
+uint16_t ppm = 100;
 
 int val1, val2, val3; 
 
@@ -20,28 +20,36 @@ HallEffectSpeedSensor he3(he3pin, ppm);
 void setup() {
   
   Serial.begin(9600);
+  Serial.println("Starting");
   he1.begin(1);
   he2.begin(1);  
   he3.begin(1);
 }
 
 void loop() {
-  
   he1.updateSensor();
   he2.updateSensor();
   he3.updateSensor();
 
   unsigned long currentTime = micros();
-  
+  Serial.println("I got here 0");
+    
   if (currentTime - previousTime > timeInterval) {
+    Serial.println("I got here 1");
+
     val1 = he1.getSpeed();
     val2 = he2.getSpeed();
     val3 = he3.getSpeed();
 
-    Serial.print("Sensor 1 speed: " + val1);
-    Serial.print("  Sensor 2 speed: " + val2);
-    Serial.println("  Sensor 3 speed: " + val3);
+    Serial.print("Sensor 1 speed: ");
+    Serial.print(val1);
+    Serial.print("  Sensor 2 speed: ");
+    Serial.print(val2);
+    Serial.print("  Sensor 3 speed: ");
+    Serial.print(val2);
+    Serial.println("The time is: "+ micros());
     previousTime = currentTime;
   }
+  
   
 }
