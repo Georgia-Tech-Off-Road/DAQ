81281489ed0613f17c305124fe5c010c406f09da
Ryan Chen <chenrkc030@gmail.com>
2021-03-11 23:11:26 -0500

    Reconstructed FreqMeasureMulti without using FreqMeasureMulti library

--- Lib/HallEffectSpeedSensor/HallEffectSpeedSensor.cpp
+++ Lib/HallEffectSpeedSensor/HallEffectSpeedSensor.cpp
@@ -1,85 +1,144 @@
 #ifndef __HALLEFFECTSPEEDSENSOR_CPP__
 #define __HALLEFFECTSPEEDSENSOR_CPP__
 
-/**
- * HallEffectSpeedSensor.cpp
- */
-
 #include "HallEffectSpeedSensor.h"
+#include "ElapsedCycles.h"
 #include <Arduino.h>
 
 
+// /**
+//  * HallEffectSpeedSensor.cpp
+//  */
+
+
+HallEffectSpeedSensor::HallEffectSpeedSensor(uint8_t pin_input, uint32_t ticks_per_revolution) :
+	_pin_input(pin_input),
+	_ticks_per_revolution(ticks_per_revolution),
+	_current_tick(0)
+{
+
+} 
+
 /**
- * Sets up the digital out pins and the PID if specified.
+ *
  */
-HallEffectSpeedSensor::HallEffectSpeedSensor(int inputPin, uint8_t toneWheelTeeth, uint16_t intervalLength = 50,uint16_t averagingAmount = 200){
-  _inputPin = inputPin;
-  _toneWheelTeeth = toneWheelTeeth; //Multiply by 2 when using FREQMEASUREMULTI_ALTERNATE mode
-  _averagingAmount = averagingAmount;
-  _intervalTime = micros();
-  _intervalLength = intervalLength;
-  _currSpeed = 0;
-  _readings = new uint16_t[averagingAmount];
-  _count = 0;
-  _sum = 0;
-  _index = 0;
-  _timeCounter = 1;
+void HallEffectSpeedSensor::setup()
+{
+	pinMode(_pin_input, INPUT_PULLUP);
+	// attachInterrupt(digitalPinToInterrupt(_pin_input), interrupt, FALLING)
+	
 }
 
-void HallEffectSpeedSensor::begin(bool useAlternate = false){ //Set to 1 to use alternate mode
-  //Only use alternate mode when tone wheel teeth width and air gap are same size.
-  if(useAlternate){
-    _freq.begin(_inputPin, FREQMEASUREMULTI_ALTERNATE);
-    _toneWheelTeeth *= 2;
-  } else {
-    _freq.begin(_inputPin);
-  }
-  for (int i = 0; i < _averagingAmount; i++) {
-    _readings[i] = 0;
-  }
+void HallEffectSpeedSensor::interrupt()
+{
+	_latest_cycle_count = _cycle_counter;
+	_cycle_counter = 0;
+	++_current_tick;
+
+	// // prevent overflow of _current_tick by checking if near max value of data type
+	// if (_current_tick>3000000000) 
+	// {
+	// 	_current_tick = _current_tick%_ticks_per_revolution;
+	// }
 }
 
-void HallEffectSpeedSensor::updateSensor(){
-  if (_freq.available()){
-    _sum += _freq.read();
-    _count++;
-  }
-
-  if (abs(micros() - _intervalTime) > _intervalLength){
-    _intervalTime = micros();
-    //If any readings calculate a new curr_speed
-    if (_count > 0) {
-    _currSpeed = _freq.countToFrequency(_sum / _count) * 60 / _toneWheelTeeth; // (1 tooth / s) * (60s / 1 min) * (1 rev / x teeth) = rpm
-    _timeCounter = 1;
-    } 
-    //If no readings set curr_speed to max possible speed without taking measurements assuming less than previous speed
-    else if(60000000 / (_toneWheelTeeth * (_intervalLength * _timeCounter)) < _currSpeed) {
-        _currSpeed = 60000000 / (_toneWheelTeeth * (_intervalLength * _timeCounter));
-        _timeCounter++;
-    }
-    else{_timeCounter++;}
-
-    _sum = 0;
-    _count = 0;
-    _readings[_index] = _currSpeed;
-
-    //Increment or reset index
-    if(_index == _averagingAmount - 1){
-      _index = 0;
-    }
-    else{
-     _index++;
-    }
-  }
+double HallEffectSpeedSensor::get_rpm()
+{
+	double dt = (double) _latest_cycle_count/_mcu_clock_speed;
+	Serial.println(_ticks_per_revolution);
+	return 1/(dt*_ticks_per_revolution)*60;		// rpm
 }
 
-int HallEffectSpeedSensor::getSpeed(){
-  int sum = 0;
-  for (int i = 0; i < _averagingAmount; i++) {
-    sum += _readings[i];
-  }
-  return sum / _averagingAmount;
+uint32_t HallEffectSpeedSensor::get_pos()
+{
+	if (_current_tick >= _ticks_per_revolution)
+	{
+		_current_tick = _current_tick%_ticks_per_revolution;
+	}
+	return _current_tick;
 }
+
+uint8_t HallEffectSpeedSensor::get_input_pin()
+{
+	return _pin_input;
+}
+
+
+// /**
+//  * Sets up the digital out pins and the PID if specified.
+//  */
+// HallEffectSpeedSensor::HallEffectSpeedSensor(int inputPin, uint8_t toneWheelTeeth, uint16_t intervalLength = 50,uint16_t averagingAmount = 200){
+//   _inputPin = inputPin;
+//   _toneWheelTeeth = toneWheelTeeth; //Multiply by 2 when using FREQMEASUREMULTI_ALTERNATE mode
+//   _averagingAmount = averagingAmount;
+//   _intervalTime = micros();
+//   _intervalLength = intervalLength;
+//   _currSpeed = 0;
+//   _readings = new uint16_t[averagingAmount];
+//   _count = 0;
+//   _sum = 0;
+//   _index = 0;
+//   _timeCounter = 1;
+// }
+
+// void HallEffectSpeedSensor::begin(bool useAlternate = false){ //Set to 1 to use alternate mode
+//   //Only use alternate mode when tone wheel teeth width and air gap are same size.
+//   if(useAlternate){
+//     _freq.begin(_inputPin, FREQMEASUREMULTI_ALTERNATE);
+//     _toneWheelTeeth *= 2;
+//   } else {
+//     _freq.begin(_inputPin);
+//   }
+//   for (int i = 0; i < _averagingAmount; i++) {
+//     _readings[i] = 0;
+//   }
+// }
+
+// void HallEffectSpeedSensor::updateSensor(){
+//   if (_freq.available()){
+//     _sum += _freq.read();
+//     _count++;
+//   }
+
+//   if (abs(micros() - _intervalTime) > _intervalLength){
+//     _intervalTime = micros();
+//     //If any readings calculate a new curr_speed
+//     if (_count > 0) {
+//     _currSpeed = _freq.countToFrequency(_sum / _count) * 60 / _toneWheelTeeth; // (1 tooth / s) * (60s / 1 min) * (1 rev / x teeth) = rpm
+//     _timeCounter = 1;
+//     } 
+//     //If no readings set curr_speed to max possible speed without taking measurements assuming less than previous speed
+//     else if(60000000 / (_toneWheelTeeth * (_intervalLength * _timeCounter)) < _currSpeed) {
+//         _currSpeed = 60000000 / (_toneWheelTeeth * (_intervalLength * _timeCounter));
+//         _timeCounter++;
+//     }
+//     else{_timeCounter++;}
+
+//     _sum = 0;
+//     _count = 0;
+//     _readings[_index] = _currSpeed;
+
+//     //Increment or reset index
+//     if(_index == _averagingAmount - 1){
+//       _index = 0;
+//     }
+//     else{
+//      _index++;
+//     }
+//   }
+// }
+
+// int HallEffectSpeedSensor::getSpeed(){
+//   int sum = 0;
+//   for (int i = 0; i < _averagingAmount; i++) {
+//     sum += _readings[i];
+//   }
+//   return sum / _averagingAmount;
+// }
+
+
+
+
 
 
 #endif
--- Lib/HallEffectSpeedSensor/HallEffectSpeedSensor.h
+++ Lib/HallEffectSpeedSensor/HallEffectSpeedSensor.h
@@ -1,56 +1,81 @@
 #ifndef __HALLEFFECTSPEEDSENSOR_H__
 #define __HALLEFFECTSPEEDSENSOR_H__
 
-/**
- * HallEffectSpeedSensor.h
- * A wrapper to work with a hall effect sensor.
- */
+#include "ElapsedCycles.h"
 
-#include <FreqMeasureMulti.h>
+// /**
+//  * HallEffectSpeedSensor.h
+//  * A wrapper to work with a hall effect sensor.
+//  */
 
 class HallEffectSpeedSensor {
-private:
-    /**
-     * 
-     */
-    uint8_t _inputPin;
-    uint16_t _currSpeed;
-    uint16_t * _readings;
-    uint32_t _intervalTime;
-    uint8_t _toneWheelTeeth;
-    uint16_t _averagingAmount;
-    uint16_t _intervalLength; //How often _currSpeed is updated in microseconds (limited by processor speed)
-    uint16_t _index;
-    uint16_t _count;
-    uint32_t _sum;
-    uint16_t _timeCounter;
-    FreqMeasureMulti _freq;
-    
-
 public:
-    /**
-     * Sets up the digital input pin for the sensor.
-     */
-    HallEffectSpeedSensor(int inputPin, uint8_t toneWheelTeeth, uint16_t intervalLength = 50,uint16_t averagingAmount = 200);
+    HallEffectSpeedSensor(uint8_t pin_input, uint32_t ticks_per_revolution);
 
-    ~HallEffectSpeedSensor(){
-        delete _readings;
-    }
+    void setup();
+    void interrupt();
 
+    double get_rpm();
+    uint32_t get_pos();
+    uint8_t get_input_pin();
 
-    /**
-     * Configures hardware
-     */
-    void begin(bool useAlternate = false);
-
-    /**
-     * Speed is in RPM
-     */
-    int getCurrSpeed(){ return _currSpeed; }
-    int getSpeed();
-
-    void updateSensor();
+private:
+    const static uint32_t _mcu_clock_speed = F_CPU;
+    const uint16_t _ticks_per_revolution;    // 
+    const uint8_t _pin_input;
+    ElapsedCycles _cycle_counter;       // number of elapsed processor clock cycles
+    uint32_t _latest_cycle_count;       // number of elapsed clock cycles between the last two interrupts
+    uint32_t _current_tick;         // tracks the current rotational position
 
 };
+
+
+
+
+
+// class HallEffectSpeedSensor {
+// private:
+//     /**
+//      * 
+//      */
+//     uint8_t _inputPin;
+//     uint16_t _currSpeed;
+//     uint16_t * _readings;
+//     uint32_t _intervalTime;
+//     uint8_t _toneWheelTeeth;
+//     uint16_t _averagingAmount;
+//     uint16_t _intervalLength; //How often _currSpeed is updated in microseconds (limited by processor speed)
+//     uint16_t _index;
+//     uint16_t _count;
+//     uint32_t _sum;
+//     uint16_t _timeCounter;
+//     FreqMeasureMulti _freq;
+    
+
+// public:
+//     /**
+//      * Sets up the digital input pin for the sensor.
+//      */
+//     HallEffectSpeedSensor(int inputPin, uint8_t toneWheelTeeth, uint16_t intervalLength = 50,uint16_t averagingAmount = 200);
+
+//     ~HallEffectSpeedSensor(){
+//         delete _readings;
+//     }
+
+
+//     /**
+//      * Configures hardware
+//      */
+//     void begin(bool useAlternate = false);
+
+//     /**
+//      * Speed is in RPM
+//      */
+//     int getCurrSpeed(){ return _currSpeed; }
+//     int getSpeed();
+
+//     void updateSensor();
+
+// };
 
 #endif
