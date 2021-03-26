fdd5a22243016ee8e669639da12b153699b265b2
Benjamin Boeckman <bboeckman3@gatech.edu>
2021-03-26 07:37:00 -0400

    Adding mostly done parser that can append to an excel sheet with varying sensors and pack sizes

--- Application/daata_v1_0/DataAcquisition/DataParser.py
+++ Application/daata_v1_0/DataAcquisition/DataParser.py
@@ -8,7 +8,7 @@ Data is stored in an excel spreadsheet.
 version: 1.0
 """
 
-dataFile = open("sample.bin", "rb")
+dataFile = open("TEST1.bin", "rb")
 
 byte = dataFile.read(1)
 
@@ -16,7 +16,21 @@ dataSheet = Workbook()
 
 sheet1 = dataSheet.add_sheet('Sheet 1')
 
+DataDict = {}
+
 while byte:
+	print(byte)
+	byte = datafile.read(1)
 
-
-    byte = dataFile.read(1)
+""" while byte:
+	sensorID = '0'
+	if byte == '1':
+		while byte == '1':
+			i = 0
+			DataDict.add(byte)
+			sheet1.write(0, i, byte)
+			i += 1
+			byte = dataFile.read(1)
+	sensorID += 1
+    byte = dataFile.read(1) """
+	
