035e0edba8086d275ce339c8ab4ab1189ba2c8d1
Andrew Hellrigel <ahellrigel33@gmail.com>
2021-11-10 22:02:06 -0500

    Updated wireless comm throughput file

--- Misc_Projects/Comms_Testing/WirelessCommsThroughput/WirelessCommsThroughput.ino
+++ Misc_Projects/Comms_Testing/WirelessCommsThroughput/WirelessCommsThroughput.ino
@@ -1,5 +1,6 @@
 #include "UARTComms.h"
 #include "SerialComms.h"
+#include "TimeSensor.h"
 
 #define LED_PIN 13
 
@@ -9,17 +10,27 @@ UARTComms uart_xbee(Serial2);
 uint32_t prev_time = micros();
 bool led_state = 0;
 
+TimeSensor ts;
+Block<uint32_t> block;
+
 void setup() {
   uart_pc.begin(115200);
   uart_xbee.begin(115200);
+  
+  uart_xbee.attach_input_block(block, 105);
+  uart_xbee.attach_output_block(ts, 100);
 
-  uart_pc.attach_throughput_comms(uart_xbee);
   uart_xbee.attach_throughput_comms(uart_pc);
+
+  uart_pc.attach_output_block(ts, 100);
+  
+  uart_pc.attach_throughput_comms(uart_xbee);
 
   pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
+  ts.update();
   uart_pc.update();
   uart_xbee.update();
 
@@ -27,5 +38,6 @@ void loop() {
     led_state = !led_state;
     digitalWrite(LED_PIN, led_state);
     prev_time = micros();
+    //Serial.println(block.get_data());
   } 
 }
