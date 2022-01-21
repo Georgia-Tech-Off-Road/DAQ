25941cf31f46c482d2014007dfeaf60388c21b6f
Andrew Hellrigel <ahellrigel33@gmail.com>
2022-01-20 20:32:34 -0500

    Added aux daq definitions file

---
+++ Auxillary_DAQ_System/2022_car/AuxDaqCode/AuxDaqDefinitions.h
@@ -0,0 +1,94 @@
+// ---- IN USE ----
+/* Below put pins that are actually in use
+ * ex. If there is and engine speed sensor on pin 5 do
+ * #define ENGINE_SPEED_PIN PIN_5
+ */
+
+// ---- Bottom ----
+
+// Pins on Left Side Of Board (Top to Bottom)
+// ADC 1
+#define ADC1_CS 33
+#define ADC1_1 1 // Top Connector
+#define ADC1_2 2
+#define ADC1_3 3
+#define ADC1_4 4
+#define ADC1_5 5
+#define ADC1_6 6
+#define ADC1_7 7
+#define ADC1_8 8
+// ADC 2
+#define ADC2_CS 34
+#define ADC2_1 1
+#define ADC2_2 2
+#define ADC2_3 3
+#define ADC2_4 4 // Bottom Connector
+
+// Pins on Left Side Of Board (Top to Bottom)
+// I2C           // Top Connector
+// I2C
+#define SPI_CS10 10
+#define SPI_CS14 14
+#define PIN_9 9
+#define PIN_8 8
+#define PIN_TX2 8
+#define PIN_7 7
+#define PIN_RX2 7
+#define PIN_6 6
+#define PIN_5 5
+#define PIN_4 4
+#define PIN_3 3
+#define PIN_2 2  // Bottom Connector
+
+
+// ---- Top Left (Analog) ----
+
+#define ADC2_8 8 // Top Connector
+#define ADC2_7 7
+#define ADC2_6 6
+#define ADC2_5 5
+#define ADC3_CS 35
+#define ADC3_8 8
+#define ADC3_7 7
+#define ADC3_6 6
+#define ADC3_5 5
+#define ADC3_4 4
+#define ADC3_3 3
+#define ADC3_2 2
+#define ADC3_1 1 // Bottom Connector
+
+// ---- Top Right (Digital) ----
+
+// I2C           // Top Connector
+// I2C
+#define SPI_CS26 26
+#define SPI_CS25 25
+#define PIN_24 24
+#define PIN_24A A10
+#define PIN_23 23
+#define PIN_23A A9
+#define PIN_22 22
+#define PIN_22A A8
+#define PIN_21 21
+#define PIN_21A A7
+#define PIN_RX5 21
+#define PIN_20 20
+#define PIN_20A A6
+#define PIN_TX5 20
+#define PIN_17 17
+#define PIN_17A A3
+#define PIN_TX4 17
+#define PIN_16 16
+#define PIN_16A A2
+#define PIN_RX4 16
+#define PIN_15 15 // Bottom Connector
+#define PIN_15A A1
+
+
+// ---- Misc Definitions ----
+
+#define LED_PIN 30
+#define LED_PIN2 31
+#define BUTTON_PIN 32
+
+
