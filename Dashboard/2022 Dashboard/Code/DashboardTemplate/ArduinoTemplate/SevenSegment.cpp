8b908478540e94a300d8fd00b784519b7f4406ba
Param Pithadia <ppithadia6@gatech.edu>
2022-02-10 21:51:13 -0500

    Dashboard Code Organization

---
+++ Dashboard/2022 Dashboard/Code/DashboardTemplate/ArduinoTemplate/SevenSegment.cpp
@@ -0,0 +1,12 @@
+//#include "SevenSegment.h"
+//
+//
+//SevenSegment::SevenSegment(Adafruit_TLC5947 &driver) : 
+//    _driver(driver) {
+//    _digit[0] = _digit[1] = _digit[2] = 0;
+//}
+//
+//void SevenSegment::set_digit(uint8_t segment, uint8_t digit){
+//    if(segment > 2 || digit > 9) return;
+//    _digit[segment] = digit;
+//}
--- Dashboard/2022 Dashboard/Code/DashboardTest/DashboardTest/DashboardTest.ino
+++ Dashboard/2022 Dashboard/Code/DashboardTest/DashboardTest/DashboardTest.ino
@@ -18,16 +18,18 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 TLC5952 testR(12, 11, 10);
 TLC5952 testL(3, 2, 4);
 
-#include <TinyGPS.h>
-#include <SoftwareSerial.h>
-TinyGPS gps;
-#define gpsPort Serial2
-char buf[32];
+//#include <TinyGPS.h>
+//#include <SoftwareSerial.h>
+//TinyGPS gps;
+//#define gpsPort Serial2
+//char buf[32];
 
-uint32_t testSpeed = 1;
+float testSpeed = 1;
 uint32_t topSpeed = 40;
 uint32_t servoSet = 0;
 uint32_t ledSet = 0;
+
+unsigned long bruh;
 
 void setup() {
   // put your setup code here, to run once:
@@ -35,10 +37,10 @@ void setup() {
   servoL.begin();
 
   Serial.begin(115200);
-  gpsPort.begin(9600);
-  Serial.print("Simple TinyGPS library v. "); Serial.println(TinyGPS::library_version());
-  Serial.println("by Mikal Hart");
-  Serial.println();
+//  gpsPort.begin(9600);
+//  Serial.print("Simple TinyGPS library v. "); Serial.println(TinyGPS::library_version());
+//  Serial.println("by Mikal Hart");
+//  Serial.println();
 
 if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
@@ -56,81 +58,96 @@ if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
-  servoSet = ((float) testSpeed / (float) topSpeed) * 270.0;
-  servoR.writeDeg(servoSet);
-  servoL.writeDeg(servoSet);
-  ledSet = (((float) testSpeed / (float) topSpeed) * 23.0);
-  ledSet = 16777216 - (pow(2, 23 - ledSet));
-  testR.set_on_off_data(ledSet);
-  testL.set_on_off_data(ledSet);
-  testR.write();
-  testL.write();
-  testSpeed += 1;
-
-  testdrawtest(testSpeed); 
-
-  bool newData = false;
-  unsigned long chars;
-  unsigned short sentences, failed;
-  // For one second we parse GPS data and report some key values
-  for (unsigned long start = millis(); millis() - start < 1000;) {
-    while (gpsPort.available()) {
-      char c = gpsPort.read();
-      // Serial.write(c); // uncomment this line if you want to see the GPS data flowing
-      if (gps.encode(c)) // Did a new valid sentence come in?
-        newData = true;
-    }
+  for (float i = 0.0; i <= 40; i += 0.25){
+    bruh = micros();
+    Serial.println(bruh);
+    servoSet = ((float) i / (float) topSpeed) * 270.0;
+    servoR.writeDeg(servoSet);
+    servoL.writeDeg(servoSet);
+    ledSet = (((float) i / (float) topSpeed) * 23.0);
+    ledSet = 16777216 - (pow(2, 23 - ledSet));
+    testR.set_on_off_data(ledSet);
+    testL.set_on_off_data(ledSet);
+    testR.write();
+    testL.write();
+    bruh = micros();
+    Serial.println(bruh);
+    testdrawtest(i);
   }
+  for (float i = 40.0; i >= 0; i -= 0.25){
+    servoSet = ((float) i / (float) topSpeed) * 270.0;
+    servoR.writeDeg(servoSet);
+    servoL.writeDeg(servoSet);
+    ledSet = (((float) i / (float) topSpeed) * 23.0);
+    ledSet = 16777216 - (pow(2, 23 - ledSet));
+    testR.set_on_off_data(ledSet);
+    testL.set_on_off_data(ledSet);
+    testR.write();
+    testL.write();
+    testdrawtest(i);
+  } 
 
- unsigned long age;
-  if (newData) {
-    float flat, flon;
-    gps.f_get_position(&flat, &flon, &age);
-    Serial.print("LAT=");
-    Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
-    Serial.print(" LON=");
-    Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
-    Serial.print(" SAT=");
-    Serial.print(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());
-    Serial.print(" PREC=");
-    Serial.print(gps.hdop() == TinyGPS::GPS_INVALID_HDOP ? 0 : gps.hdop());
-    //GPS mode
-    Serial.print(" Constellations=");
-    Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0 : gps.constellations());
-  }
-
-  //satellites in view
-  uint32_t* satz = gps.trackedSatellites();
-  uint8_t sat_count = 0;
-  for(int i=0;i<24;i++) {
-    if(satz[i] != 0) {   //exclude zero SNR sats
-      sat_count++;
-      byte strength = (satz[i]&0xFF)>>1;
-      byte prn = satz[i]>>8;
-      sprintf(buf, "PRN %d: ", prn);
-      Serial.print(buf);
-      Serial.print(strength);
-      Serial.println("dB");
-    }
-  }
-  
-  //date time
-  int year;
-  uint8_t month, day, hour, minutes, second, hundredths;
-  gps.crack_datetime(&year, &month, &day, &hour, &minutes, &second, &hundredths, &age);
-  sprintf(buf,"GPS time: %d/%02d/%02d %02d:%02d:%02d", year, month, day, hour, minutes, second);
-  Serial.println(buf);
-
-  gps.stats(&chars, &sentences, &failed);
-  Serial.print(" CHARS=");
-  Serial.print(chars);
-  Serial.print(" SENTENCES=");
-  Serial.print(sentences);
-  Serial.print(" CSUM ERR=");
-  Serial.println(failed);
-  if (chars == 0)
-  Serial.println("** No characters received from GPS: check wiring **");
+//  bool newData = false;
+//  unsigned long chars;
+//  unsigned short sentences, failed;
+//  // For one second we parse GPS data and report some key values
+//  for (unsigned long start = millis(); millis() - start < 1000;) {
+//    while (gpsPort.available()) {
+//      char c = gpsPort.read();
+//      // Serial.write(c); // uncomment this line if you want to see the GPS data flowing
+//      if (gps.encode(c)) // Did a new valid sentence come in?
+//        newData = true;
+//    }
+//  }
+//
+// unsigned long age;
+//  if (newData) {
+//    float flat, flon;
+//    gps.f_get_position(&flat, &flon, &age);
+//    Serial.print("LAT=");
+//    Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
+//    Serial.print(" LON=");
+//    Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
+//    Serial.print(" SAT=");
+//    Serial.print(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());
+//    Serial.print(" PREC=");
+//    Serial.print(gps.hdop() == TinyGPS::GPS_INVALID_HDOP ? 0 : gps.hdop());
+//    //GPS mode
+//    Serial.print(" Constellations=");
+//    Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0 : gps.constellations());
+//  }
+//
+//  //satellites in view
+//  uint32_t* satz = gps.trackedSatellites();
+//  uint8_t sat_count = 0;
+//  for(int i=0;i<24;i++) {
+//    if(satz[i] != 0) {   //exclude zero SNR sats
+//      sat_count++;
+//      byte strength = (satz[i]&0xFF)>>1;
+//      byte prn = satz[i]>>8;
+//      sprintf(buf, "PRN %d: ", prn);
+//      Serial.print(buf);
+//      Serial.print(strength);
+//      Serial.println("dB");
+//    }
+//  }
+//  
+//  //date time
+//  int year;
+//  uint8_t month, day, hour, minutes, second, hundredths;
+//  gps.crack_datetime(&year, &month, &day, &hour, &minutes, &second, &hundredths, &age);
+//  sprintf(buf,"GPS time: %d/%02d/%02d %02d:%02d:%02d", year, month, day, hour, minutes, second);
+//  Serial.println(buf);
+//
+//  gps.stats(&chars, &sentences, &failed);
+//  Serial.print(" CHARS=");
+//  Serial.print(chars);
+//  Serial.print(" SENTENCES=");
+//  Serial.print(sentences);
+//  Serial.print(" CSUM ERR=");
+//  Serial.println(failed);
+//  if (chars == 0)
+//  Serial.println("** No characters received from GPS: check wiring **");
 } // End of main loop()
 
 // Draw characters of the default font
@@ -178,5 +195,5 @@ void testdrawtest(uint32_t displayText) {
   display.println("Speed: " + String(displayText));
 
   display.display();
-  delay(2000);
+  //delay(2000);
 }
