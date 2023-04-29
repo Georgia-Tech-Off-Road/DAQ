3b1f15f5a51123c18b9c19ab9f83a2ed1ca88fce
Vishnav Deenadayalan <vishnavdeena@gmail.com>
2023-04-29 14:40:10 -0400

    auxDaq code used for testing days in April

---
+++ Auxillary_DAQ_System/2023_car/AuxDaqCode/TestingDayApril21_23/TestingDayApril21_23.h
@@ -0,0 +1,54 @@
+#ifndef TESTINGDAYNOV19_H
+#define TESTINGDAYNOV19_H
+
+// Communication Libraries
+#include "SerialComms.h"
+#include "WirelessComms.h"
+#include "SDComms.h"
+#include "BlockId.h"
+
+// Sensor Libraries
+#include "TeensyADC.h"
+#include "ADS8688.h"
+#include "SpeedSensor.h"
+#include "LDS.h"
+#include "IMUSensor.h"
+#include "TimeSensor.h"
+#include "VoltageSensor.h"
+#include "DigitalSensor.h"
+#include "BrakePressureSensor.h"
+
+// Control Libraries
+#include "DigitalOutput.h"
+
+// Utility
+#include "ClockTimer.h"
+#include "EdgeDetector.h"
+
+// Pins and Constants
+#define ADC1 33
+#define ADC2 34
+#define ADC3 35
+
+#define LDS_PEDAL1        0
+#define LDS_PEDAL2        1
+#define LDS_PEDAL3        2
+#define LDS_PEDAL4        3
+#define LDS_PEDAL_STROKE1 200
+#define LDS_PEDAL_STROKE2 225
+#define BRAKE_FRONT 4 // ADC3
+#define BRAKE_REAR  5 // ADC3
+
+#define SPEED_ENGINE        2
+#define SPEED_ENGINE_PPR    600
+#define SPEED_SECONDARY     22
+#define SPEED_SECONDARY_PPR 30
+
+#define IMU 26
+
+#define LED_ONBOARD       30
+#define LED_PANEL_WHITE   9
+#define LED_PANEL_RED     8
+#define BTN_PANEL         7
+
+#endif
